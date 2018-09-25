3ecf5ea3ad8c9f4ca20dd3a5264f4089074a34c0
oscar bonilla <ovbonilla@yaho.com>
2018-09-25 15:50:52 -0500

    curso 25/09/2018 formulario js

--- SCRIPTS/app.js
+++ SCRIPTS/app.js
@@ -1,4 +1,4 @@
-/*definir variables, nunca deben iniciar con numero*/
+/*definir variables, nunca deben iniciar con numero
 var numero1=10.5;
 var numero2=20;
 var mul = numero1*numero2;
@@ -20,11 +20,11 @@ var profesion ="Desarrollador Full Stack";
 
 console.log(nombre+profesion);
 
-/*definimos una variable booleana*/
+/*definimos una variable booleana
 var tiene_mascota = true;
 /*console.log(tiene_mascota);
 
-/*definimos un array*/
+/*definimos un array
 var datos=["oscar",50,true,79471430,"kra 36 calle 1f"];
 /*console.log(datos[4]);
 
@@ -45,7 +45,7 @@ estructura
  if (condicion){
 	
  }
-*/
+
 var edad=41;
 
 if (edad>=18 && edad <=40) {
@@ -72,9 +72,9 @@ for(var i=0;i<7;i++)
 
 /*FUNCIONES
 function nombre (){}
-*/
 
-/*
+
+
 function sumar(numero1,numero2){
 	var resultado=numero1+numero2;
 	return resultado;
@@ -84,13 +84,53 @@ var res=sumar(88,55);
 console.log("el resultado es",res);
 */
 
-function iva(precio){
-	var riva=precio*(19/100);
-	return riva;
+function iva(precio_producto,porcentaje_iva){
+	var iva_producto=precio_producto*(porcentaje_iva/100);
+	return iva_producto;
 }
 
+function calcular_iva(){
+	var precio=parseInt(document.getElementById("precio").value);
+	var porcentaje_iva=parseInt(document.getElementById("iva").value);
+
+/*llamamos funcion iva dentro de la funcion calcular_iva*/
+	var resultado=iva(precio, porcentaje_iva);
+	var salida=document.getElementById('resultado');
+	var texto=document.createTextNode(resultado);
+	/*salida.appendChild(texto);*/
+	salida.value=resultado;
+
+
+
+console.log (resultado);
+}
+
+
+
+/*
 var resultado=iva(100000);
 console.log("EL IVA DEL PRODUCTO ES   -->  ",resultado);
+
+
+/*optener los elementosa de una etiqueta
+
+var titulos = document.getElementsByTagName('h1');
+
+console.log (titulos);
+
+console.log(titulos[0]);
+
+var clasetexto= document.getElementsByClassName('texto');
+
+console.log (clasetexto);
+
+
+var sede= document.getElementById('sede');
+
+console.log(sede);
+
+/*formularios  */
+
 
 
 
